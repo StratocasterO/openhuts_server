7ad0ec90f222d816a5b03cf2d6ba105264a5948d
StratocasterO <omarolmedoferrer@gmail.com>
2019-09-27 13:26:31 +0200

    first commit. database and old server

---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var database = require('./refugis_db');
+
+router.get('/new', function(req, res, next) {
+
+});
+
+router.get('/fetch', function(req, res, next) {
+
+});
+
+module.exports = router;
