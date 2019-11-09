f5be1e0c5a1e8a5b27c2f6ffd0af30c40db76579
Josep Anguera <josep.anguera@gmail.com>
2019-11-09 19:17:02 +0100

    Read DB configuration from env vars by default

--- routes/openhutsdb.js
+++ routes/openhutsdb.js
@@ -1,12 +1,10 @@
 var mysql = require('mysql');
 
-//  mysql://bd9fdeeb519896:9e198052@eu-cdbr-west-02.cleardb.net/heroku_5e50e45fde7f817?reconnect=true
-
 var conexion = mysql.createConnection({
-    host:'openhutsdb.c1bcj6q0pxm2.eu-west-3.rds.amazonaws.com',
-    user:'admin',
-    password:'12345678',
-    database:'openhuts'
+    host: process.env.DB_HOST || 'pablomonteserin.com',
+    user: process.env.DB_USER || 'openhuts',
+    password: process.env.DB_PASSWORD || 'Kl0xxY0c',
+    database: 'openhuts'
 });
 
 conexion.connect(function (error){
