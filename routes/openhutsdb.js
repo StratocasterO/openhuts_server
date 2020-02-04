3ba744442b5966d695abc372f94c9ba6da4670a7
stratocastero <omarolmedoferrer@gmail.com>
2020-02-04 15:28:33 +0100

    minor changes in db connection

--- routes/openhutsdb.js
+++ routes/openhutsdb.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var conexion = mysql.createConnection({
-    host:'openhutsdb.c1bcj6q0pxm2.eu-west-3.rds.amazonaws.com',
+    host:'ohdatabase.c1bcj6q0pxm2.eu-west-3.rds.amazonaws.com',
     user:'admin',
-    password:'12345678',
+    password:'gotonature!',
     database:'openhuts'
 });
 
